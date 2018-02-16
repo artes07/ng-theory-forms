864fdbdbf8165ecc6485b9d78ecefa45bb42c186
artes07 <artes07333@gmail.com>
2018-02-16 18:04:20 +0200

    add skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  answers = [{
+    type: 'yes',
+    text: 'да'
+  }, {
+      type: 'no',
+      text: 'нет'
+  }];
 }
