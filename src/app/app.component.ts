5582ed88b8aa96904b51b6cfbd1a9658192b9348
artes07 <artes07333@gmail.com>
2018-02-19 11:33:02 +0200

    add validation to form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  @ViewChild('form') form: NgForm;
+
   answers = [{
     type: 'yes',
     text: 'да'
@@ -12,4 +15,26 @@ export class AppComponent {
       type: 'no',
       text: 'нет'
   }];
+
+  defaultAnswer = 'no';
+  defaultCountry = 'ru';
+
+  addRandEmail() {
+    const randEmail = 'test@gmail.com';
+    /*this.form.setValue({
+      user: {
+        pass: '',
+        email: randEmail
+      },
+      country: '',
+      answer: ''
+    });*/
+    this.form.form.patchValue({
+      user: { email: randEmail }
+    });
+  }
+
+  submitForm() {
+    console.log('Submited!', this.form);
+  }
 }
